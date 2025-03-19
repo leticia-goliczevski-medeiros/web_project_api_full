6782d71d631c110379d8d0061929b4ad94176371
leticia-goliczevski-medeiros <leticia_goliczevski@outlook.com>
2025-03-19 15:00:00 -0300

    refactor: make name, avatar and link optional in user schema and add default values

--- backend/models/user.js
+++ backend/models/user.js
@@ -4,19 +4,19 @@ const validator = require('validator');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
+    default: "Jacques Cousteau"
   },
   about: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
+    default: "Explorer"
   },
   avatar: {
     type: String,
-    required: true,
+    default: "https://practicum-content.s3.us-west-1.amazonaws.com/resources/moved_avatar_1604080799.jpg",
     validate: {
       validator(v) {
         return validator.isURL(v);
