0c632d1e2ea63ab93c37823ec6601ed2b3adf46d
leticia-goliczevski-medeiros <leticia_goliczevski@outlook.com>
2025-03-20 08:25:47 -0300

    feat: add /signin and /signup routes

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { celebrate, Joi } = require('celebrate');
+const { createUser, login } = require('./controllers/users');
 const { cardsRouter } = require('./routes/cards');
 const { userRouter } = require('./routes/users');
 
@@ -17,6 +19,17 @@ app.use((req, res, next) => {
 
   next();
 });
+
+app.post('/signin', login);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().uri(),
+    email: Joi.string().email().required(),
+    password: Joi.string().required().min(8),
+  })
+}), createUser);
 
 app.use('/cards', cardsRouter);
 app.use('/users', userRouter);
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,24 +1,13 @@
 const express = require('express');
 const {
-  getUsers, getUser, createUser, updateProfileInfo, updateProfileAvatar,
+  getUsers, getUser, updateProfileInfo, updateProfileAvatar,
 } = require('../controllers/users');
-const { celebrate, Joi } = require('celebrate');
 
 const userRouter = express.Router();
 
 userRouter.get('/', getUsers);
 
 userRouter.get('/:id', getUser);
-
-userRouter.post('/', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().uri(),
-    email: Joi.string().email().required(),
-    password: Joi.string().required().min(8),
-  })
-}), createUser);
 
 userRouter.patch('/me', updateProfileInfo);
 
