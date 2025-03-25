7a0909fdb4201f4a4a251cc0e23f91d97b9fa464
leticia-goliczevski-medeiros <leticia_goliczevski@outlook.com>
2025-03-25 20:27:26 -0300

    feat: add request validation to card routes

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -2,17 +2,36 @@ const express = require('express');
 const {
   getCards, createCard, deleteCard, likeCard, dislikeCard,
 } = require('../controllers/cards');
+const { Joi, celebrate } = require('celebrate');
+const { validateURL } = require('../middlewares/validation');
 
 const cardsRouter = express.Router();
 
 cardsRouter.get('/', getCards);
 
-cardsRouter.post('/', createCard);
+cardsRouter.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).required(),
+    link: Joi.string().custom(validateURL).required()
+  })
+}), createCard);
 
-cardsRouter.delete('/:id', deleteCard);
+cardsRouter.delete('/:id', celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().required(),
+  })
+}), deleteCard);
 
-cardsRouter.put('/:id/likes', likeCard);
+cardsRouter.put('/:id/likes', celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().required(),
+  })
+}), likeCard);
 
-cardsRouter.delete('/:id/likes', dislikeCard);
+cardsRouter.delete('/:id/likes', celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().required(),
+  })
+}), dislikeCard);
 
 module.exports = { cardsRouter };
