8dbdcee1c5f53f033cb0a22572b32fb4c31b76c5
leticia-goliczevski-medeiros <leticia_goliczevski@outlook.com>
2025-03-26 13:30:19 -0300

    feat: implement request logger and error logger

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const { cardsRouter } = require('./routes/cardsRoutes');
 const { userRouter } = require('./routes/usersRoutes');
 const { authRouter } = require('./routes/authRoutes');
 const { errors } = require('celebrate');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { authorize } = require('./middlewares/auth');
 const { errorHandler } = require('./middlewares/errorHandler');
 
@@ -14,14 +15,18 @@ mongoose.connect('mongodb://0.0.0.0:27017/aroundb');
 
 app.use(express.json());
 
+app.use(requestLogger);
+
 app.use(authRouter);
 
 app.use(authorize, cardsRouter);
 app.use(authorize, userRouter);
 
-app.use('/', (req, res) => {
-  res.status(404).send({ message: 'A solicitação não foi encontrada' });
-});
+// app.use('/', (req, res) => {
+//   res.status(404).send({ message: 'A solicitação não foi encontrada' });
+// });
+
+app.use(errorLogger);
 
 app.use(errors());
 
