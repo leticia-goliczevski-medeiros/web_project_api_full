78a4b3dc03bcaf14894aa60c8201e94dde7551fc
leticia-goliczevski-medeiros <leticia_goliczevski@outlook.com>
2025-03-25 21:39:05 -0300

    feat: add request validation to user routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,14 +2,24 @@ const express = require('express');
 const {
   getUserInfo, updateProfileInfo, updateProfileAvatar,
 } = require('../controllers/users');
-const { authorize } = require('../middlewares/auth');
+const { Joi, celebrate } = require('celebrate');
+const { validateURL } = require('../middlewares/validation');
 
 const userRouter = express.Router();
 
-userRouter.get('/me', authorize, getUserInfo);
+userRouter.get('/me', getUserInfo);
 
-userRouter.patch('/me', authorize, updateProfileInfo);
+userRouter.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30)
+  })
+}), updateProfileInfo);
 
-userRouter.patch('/me/avatar', authorize, updateProfileAvatar);
+userRouter.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().custom(validateURL)
+  })
+}), updateProfileAvatar);
 
 module.exports = { userRouter };
