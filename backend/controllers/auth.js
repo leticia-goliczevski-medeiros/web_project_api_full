f7349fe12afe99cf3cdfaff3ff22030ac07a117c
leticia-goliczevski-medeiros <leticia_goliczevski@outlook.com>
2025-04-01 09:43:14 -0300

    refactor: modify createUser parameters to only email and password

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -19,16 +19,16 @@ function login(req, res, next) {
 
 function createUser(req, res, next) {
   const {
-    name, about, avatar, email, password,
+    email, password,
   } = req.body;
 
   bcrypt.hash(password, 10)
     .then((hash) => User.create({
-      name, about, avatar, email, password: hash,
+      email, password: hash,
     }))
     .then((user) => {
       if (!user) {
-        throw new ServerError(`Não foi possível criar o usuário ${name}`);
+        throw new ServerError(`Não foi possível criar o usuário.`);
       }
 
       res.status(201).send({ email: user.email, _id: user._id });
